e67e165eced6b57fc180166f9e3a8031da13200d
HongYang Gao <hongyang.gao@gmail.com>
2019-03-02 17:23:23 -0800

    Remove unique from artistid in songs table

---
+++ db/migrate/20190303012030_update_song_index.rb
@@ -0,0 +1,6 @@
+class UpdateSongIndex < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :songs, :artist_id
+    add_index :songs, :artist_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_220536) do
+ActiveRecord::Schema.define(version: 2019_03_03_012030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_220536) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_songs_on_artist_id", unique: true
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
   create_table "users", force: :cascade do |t|
