e7454ca4347fbf0e21de5dcf2c09c6e9d7f0fb04
HongYang Gao <hongyang.gao@gmail.com>
2019-03-04 11:10:25 -0800

    Edit user show container

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -5,38 +5,48 @@ import Layout from '../layout/layout';
 class UserShow extends React.Component {
 
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     componentDidMount() {
+        let id = this.props.match.params.userId;
+        this.props.fetchUser(id);
     }
 
     render() {
-        const user = this.props.currentUser;
-        return (
-            <Layout >
-                <div className='show'>
-                    <div className='show-banner'>
-                        <div className='profile-pic'>
-                        {/* this needs to be the actual user pic not current user */}
-                            <img className='user-img' src={''} alt=""/> 
-                          
+        // this user should be the fetched user not current user
+        const user = this.props.user;
+        if (user === undefined) {
+            return (
+                <div></div>
+            )
+        } else {
+            return (
+                <Layout >
+                    <div className='show'>
+                        <div className='show-banner'>
+                            <div className='profile-pic'>
+                            {/* this needs to be the actual user pic not current user */}
+                                <img className='user-img' src={''} alt=""/> 
+                              
+                            </div>
+                            <div className='display-name'>
+                                <h2>{user.username}</h2>
+                            </div>
                         </div>
-                        <div className='display-name'>
-                            <h2>{user.username}</h2>
+                        <div className='show-content'>
+                            <div className='labels'>
+                                <span>Tracks</span>
+                            </div>
+                            <div className='userssongs'>
+    
+                            </div>
                         </div>
                     </div>
-                    <div className='show-content'>
-                        <div className='labels'>
-                            <span>Tracks</span>
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
-                </div>
-            </Layout>
-        )
+                </Layout>
+            )
+            
+        }
     }
 };
 
--- frontend/components/user/user_show_container.js
+++ frontend/components/user/user_show_container.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import UserShow from './user_show';
+import {fetchUser} from '../../actions/user_actions';
 
-const msp = state => ({
-    currentUser: state.entities.users[state.session.id]
+
+const msp = (state, ownProps) => ({
+    user: state.entities.users[ownProps.match.params.userId]
 });
 
 const mdp = dispatch => ({
+    fetchUser: id => dispatch(fetchUser(id))
 });
 
 export default connect(msp, mdp)(UserShow);
