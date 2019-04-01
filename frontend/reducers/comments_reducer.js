34724d6937d94806aad5bb9d7823de430dec1cc0
HongYang Gao <hongyang.gao@gmail.com>
2019-04-01 12:04:42 -0700

    Add comment reducer

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,20 @@
+import {RECEIVE_COMMENT, REMOVE_COMMENT} from '../actions/comment_actions';
+import {RECEIVE_ONE_SONG} from '../actions/song_actions';
+
+const CommentsReducer = (oldState ={}, action) => {
+    Object.freeze(oldState);
+    let newState = Object.assign({}, oldState);
+    switch (action.type) {
+        case RECEIVE_COMMENT:
+            return Object.assign(newState, {[action.comment.id]: action.comment});
+        case REMOVE_COMMENT:
+            delete newState[action.commentId.id];
+            return newState;
+        case RECEIVE_ONE_SONG:
+            return Object.assign(newState, action.payload.comments);
+        default:
+            return oldState;
+    }
+};
+
+export default CommentsReducer;
