5bb1291cab98677eecf8cf3ea712a041aac9e5c1
HongYang Gao <hongyang.gao@gmail.com>
2019-04-01 11:34:52 -0700

    Create comment actions

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,22 @@
+import * as CommentAPI from '../util/comment_util';
+
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+const receiveComment = comment => ({
+    type: RECEIVE_COMMENT,
+    comment
+});
+
+const removeComment = commentId => ({
+    type: REMOVE_COMMENT,
+    commentId
+});
+
+export const createComment = comment => dispatch => (
+    CommentAPI.createComment(comment).then(comment => dispatch(receiveComment(comment)))
+);
+
+export const deleteComment = commentId => dispatch => (
+    CommentAPI.deleteComment(commentId).then(commentId => dispatch(removeComment(commentId)))
+);
